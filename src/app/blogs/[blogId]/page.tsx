2adca1723a5761ea1f7d50126cdd3878476d7408
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-02-03 01:36:56 +0600

    generateStaticParams()

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,4 +1,5 @@
 import BlogDetails from '@/components/ui/BlogDetails';
+import { TBlog } from '@/types';
 
 type TBlogId = {
   params: {
@@ -6,12 +7,20 @@ type TBlogId = {
   };
 };
 
+export const generateStaticParams = async () => {
+  const res = await fetch(`http://localhost:5000/blogs`);
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: TBlog) => ({
+    blogId: blog.id,
+  }));
+};
+
 const BlogDetailPage = async ({ params }: TBlogId) => {
   const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`, {
     cache: 'no-store',
   });
   const blog = await res.json();
-  console.log(blog);
+  //   console.log(blog);
   return (
     <div className='my-10'>
       <BlogDetails blog={blog} />
