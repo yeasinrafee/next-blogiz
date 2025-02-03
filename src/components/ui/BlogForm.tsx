2e40d67f072bbb4e1dbe386234ec7ea496ab8819
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-02-03 12:02:53 +0600

    Creating blog

--- src/components/ui/BlogForm.tsx
+++ src/components/ui/BlogForm.tsx
@@ -1,6 +1,7 @@
-"use client";
+'use client';
 
-import { useForm } from "react-hook-form";
+import { createBlog } from '@/actions/createBlog';
+import { useForm } from 'react-hook-form';
 
 type FormValues = {
   id: string;
@@ -20,78 +21,89 @@ const CreateBlogForm = () => {
   } = useForm<FormValues>();
 
   const onSubmit = async (data: FormValues) => {
-    console.log(data);
+    const res = await fetch('http://localhost:5000/blogs');
+    const blogs = await res.json();
+    data.id = JSON.stringify(blogs.length + 1);
+    data.total_likes = '100';
+
+    try {
+      const res = await createBlog(data);
+      console.log(res);
+    } catch (err: any) {
+      console.log(err.message);
+      throw new Error(err.message);
+    }
   };
 
   return (
-    <div className="my-10">
-      <h1 className="text-center text-4xl">
-        Post Your <span className="text-accent">Blog</span>
+    <div className='my-10'>
+      <h1 className='text-center text-4xl'>
+        Post Your <span className='text-accent'>Blog</span>
       </h1>
 
-      <div className="hero min-h-screen">
-        <div className="card w-[50%] shadow-xl bg-base-100">
-          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Title</span>
+      <div className='hero min-h-screen'>
+        <div className='card w-[50%] shadow-xl bg-base-100'>
+          <form onSubmit={handleSubmit(onSubmit)} className='card-body'>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text'>Title</span>
               </label>
               <input
-                type="text"
-                {...register("title")}
-                placeholder="Title"
-                className="input input-bordered"
+                type='text'
+                {...register('title')}
+                placeholder='Title'
+                className='input input-bordered'
                 required
               />
             </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Description</span>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text'>Description</span>
               </label>
               <textarea
-                {...register("description")}
-                placeholder="Description"
-                className="textarea textarea-bordered"
+                {...register('description')}
+                placeholder='Description'
+                className='textarea textarea-bordered'
                 required
               />
             </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Publish Date</span>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text'>Publish Date</span>
               </label>
               <input
-                {...register("publish_date")}
-                type="date"
-                className="input input-bordered"
+                {...register('publish_date')}
+                type='date'
+                className='input input-bordered'
                 required
               />
             </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Author Name</span>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text'>Author Name</span>
               </label>
               <input
-                type="text"
-                {...register("author_name")}
-                placeholder="Author Name"
-                className="input input-bordered"
+                type='text'
+                {...register('author_name')}
+                placeholder='Author Name'
+                className='input input-bordered'
                 required
               />
             </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Blog Image</span>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text'>Blog Image</span>
               </label>
               <input
-                type="url"
-                {...register("blog_image")}
-                placeholder="Image URL"
-                className="input input-bordered"
+                type='url'
+                {...register('blog_image')}
+                placeholder='Image URL'
+                className='input input-bordered'
                 required
               />
             </div>
-            <div className="form-control mt-6">
-              <button type="submit" className="btn btn-accent btn-outline">
+            <div className='form-control mt-6'>
+              <button type='submit' className='btn btn-accent btn-outline'>
                 Post
               </button>
             </div>
